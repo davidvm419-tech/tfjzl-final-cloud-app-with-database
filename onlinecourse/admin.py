9f4d662c3713c8a1820a24da4d7dc983fbb21310
davidvm419-tech <davidvm419@outlook.com>
2026-05-04 16:21:19 -0400

    Added task 3.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,44 +1,44 @@
 
-	from django.contrib import admin
-	# <HINT> Import any new Models here
-	from .models import Course, Choice, Lesson, Instructor, Learner, Question, Submission
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Course, Choice, Lesson, Instructor, Learner, Question, Submission
 
-	# <HINT> Register QuestionInline and ChoiceInline classes here
-	class ChoiceInline(admin.StackedInline):
-		model = Choice
-		extra = 2
-
-
-	class LessonInline(admin.StackedInline):
-		model = Lesson
-		extra = 5
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
 
-	class QuestionInline(admin.StackedInline):
-		model = Question
-		extra = 2
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
 
 
-	# Register your models here.
-	class CourseAdmin(admin.ModelAdmin):
-		inlines = [LessonInline]
-		list_display = ('name', 'pub_date')
-		list_filter = ['pub_date']
-		search_fields = ['name', 'description']
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
-	class QuestionAdmin(admin.ModelAdmin):
-		inlines = [ChoiceInline]
-		list_display = ['content']
 
-	class LessonAdmin(admin.ModelAdmin):
-		list_display = ['title']
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
 
-	# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
-	admin.site.register(Course, CourseAdmin)
-    admin.site.register(Choice)
-	admin.site.register(Lesson, LessonAdmin)
-	admin.site.register(Instructor)
-	admin.site.register(Learner)
-	admin.site.register(Question, QuestionAdmin)
-	admin.site.register(Submission)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+# <HINT> Register Question and Choice models here
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Choice)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Submission)
